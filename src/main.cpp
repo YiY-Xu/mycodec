031434b0984e1d818dbd8f975a6dfb267f4f4d52
Yi <>
2021-07-05 22:59:38 -0400

    add ffmpeg

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #define GL_SILENCE_DEPRECATION  
-#include<GLFW/glfw3.h>
+#include <GLFW/glfw3.h>
+
+bool load_frame(const char* filename, int* width, int* height, unsigned char** data);
 
 int main(int argc, const char** argv) {
     GLFWwindow *window;
@@ -17,21 +19,51 @@ int main(int argc, const char** argv) {
         return 1;
     }
 
-    unsigned char* data = new unsigned char[100 * 100 * 3];
-
-    for (int y = 0; y < 100; ++y) {
-        for (int x = 0; x < 100; ++x) {
-            data[ y * 100 * 3 + x * 3 ] = 0xff;
-            data[ y * 100 * 3 + x * 3 + 1 ] = 0x00;
-            data[ y * 100 * 3 + x * 3 + 2 ] = 0x00 ;
-        }
+    int frame_width, frame_height;
+    unsigned char* frame_data;
+    if(!load_frame("/Users/yiy/Downloads/265.h265", &frame_width, &frame_height, &frame_data)) {
+        printf("Couldn't load video frame\n");
+        return 1;
     }
 
     glfwMakeContextCurrent(window);
+
+    //Make a texture
+    GLuint tex_handle;
+    glGenTextures(1, &tex_handle);
+    glBindTexture(GL_TEXTURE_2D, tex_handle);
+
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, frame_width, frame_height, 0, GL_RGB, GL_UNSIGNED_BYTE, frame_data);
+    glBindTexture(GL_TEXTURE_2D, 0);
     while(!glfwWindowShouldClose(window)) {
         //Rendering
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glDrawPixels(100, 100, GL_RGB, GL_UNSIGNED_BYTE, data);
+        
+        int window_width, window_height;
+        glfwGetFramebufferSize(window, &window_width, &window_height);
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        glOrtho(0, window_width, 0, window_height, -1, 1);
+        glMatrixMode(GL_MODELVIEW);
+
+        // Render my own stuff
+        glEnable(GL_TEXTURE_2D);
+        glBindTexture(GL_TEXTURE_2D, tex_handle);
+        glBegin(GL_QUADS);
+            glTexCoord2d(0, 0); glVertex2i(200, 200);
+            glTexCoord2d(1, 0); glVertex2i(200 + frame_width, 200);          
+            glTexCoord2d(1, 1); glVertex2i(200 + frame_width, 200 + frame_height);
+            glTexCoord2d(0, 1); glVertex2i(200, 200 + frame_height);
+        glEnd();
+        glDisable(GL_TEXTURE_2D);
+
         glfwSwapBuffers(window);
         glfwWaitEvents();
     }
